af204a89179c67675fd89068f64c029ec870e6fe
KamilDemkowicz <kdemkowicz@genesis.com.mt>
2020-06-13 20:04:16 +0200

    Add lombok

---
+++ src/main/java/points/transforming/app/server/models/history/HistoryChanges.java
@@ -0,0 +1,14 @@
+package points.transforming.app.server.models.history;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class HistoryChanges {
+
+}
